efac806490884e9d71810a8c30f12b8ff6d0cf73
MATHEMATICS TERRAINS <amankumarpandey2397@gmail.com>
2022-01-13 11:01:50 +0530

    navigation 1 and 2 mobile view

---
+++ app/assets/js/main.js
@@ -0,0 +1,9 @@
+const hamburger = document.querySelector(".circle-hamburger");
+const navMenu = document.querySelector(".header__list");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+      hamburger.classList.toggle("active");
+      navMenu.classList.toggle("active");
+}
